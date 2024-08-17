7eac67a24130975db46acd5e97ec2cc96d2ac0c5
Adonis Cesar Legón Campo <alegon@gmail.com>
2024-08-17 00:51:53 -0400

    minor fix

--- src/test/java/app/alegon/aws/sct/migrator/AwsSctDataMigratorSchemaTests.java
+++ src/test/java/app/alegon/aws/sct/migrator/AwsSctDataMigratorSchemaTests.java
@@ -3,7 +3,6 @@ package app.alegon.aws.sct.migrator;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
