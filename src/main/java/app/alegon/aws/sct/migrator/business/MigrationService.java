e31cd15a821fbd7bd0e65a1ed129b72c8bba501d
Adonis Cesar Legón Campo <alegon@gmail.com>
2024-08-20 23:38:50 -0400

    minor changes

--- src/main/java/app/alegon/aws/sct/migrator/business/MigrationService.java
+++ src/main/java/app/alegon/aws/sct/migrator/business/MigrationService.java
@@ -19,64 +19,64 @@ import app.alegon.aws.sct.migrator.persistence.exception.DataTransporterSendExce
 @Service
 public class MigrationService {
 
-        private DataExtractor dataExtractor;
+    private DataExtractor dataExtractor;
 
-        private DataTransporter dataTransporter;
+    private DataTransporter dataTransporter;
 
-        private DataLoader dataLoader;
+    private DataLoader dataLoader;
 
-        private MigrationObserver migrationObserver;
+    private MigrationObserver migrationObserver;
 
-        public MigrationService(DataExtractor dataExtractor, DataLoader dataLoader, DataTransporter dataTransporter,
-                        MigrationObserver migrationObserver) {
-                this.dataExtractor = dataExtractor;
-                this.dataLoader = dataLoader;
-                this.dataTransporter = dataTransporter;
-                this.migrationObserver = migrationObserver;
+    public MigrationService(DataExtractor dataExtractor, DataLoader dataLoader, DataTransporter dataTransporter,
+            MigrationObserver migrationObserver) {
+        this.dataExtractor = dataExtractor;
+        this.dataLoader = dataLoader;
+        this.dataTransporter = dataTransporter;
+        this.migrationObserver = migrationObserver;
+    }
+
+    public void migrate(MigrationProject migrationProject)
+            throws DataMigrationException, DataTransporterSendException,
+            DataLoaderException, DataTransporterRemoveException {
+
+        migrationObserver
+                .onProjectMigrationStatus(
+                        new MigrationStatus(migrationProject, MigrationStatusType.InProgress, 0,
+                                null));
+
+        for (MigrationMapping migrationMapping : migrationProject.migrationMappings()) {
+            migrationObserver.onTableMigrationStatus(
+                    new MigrationMappingStatus(migrationMapping, MigrationMappingStatusType.Started,
+                            0, null));
+
+            try {
+                migrationObserver.onTableMigrationStatus(
+                        new MigrationMappingStatus(migrationMapping,
+                                MigrationMappingStatusType.InProgress, 0, null));
+                String sourceTableData = dataExtractor.extractTable(migrationMapping.sourceTable(),
+                        migrationProject.sourceDataSource());
+
+                dataTransporter.receiveTableData(sourceTableData, migrationMapping.sourceTable());
+
+                String targetTableData = dataTransporter.sendTableData(migrationMapping.sourceTable());
+                dataLoader.loadTable(targetTableData, migrationMapping.targetTable(),
+                        migrationProject.targetDataSource());
+
+                dataTransporter.removeTableData(migrationMapping.sourceTable());
+
+                migrationObserver.onTableMigrationStatus(
+                        new MigrationMappingStatus(migrationMapping,
+                                MigrationMappingStatusType.Successful, 100, null));
+            } catch (Exception e) {
+                migrationObserver.onTableMigrationStatus(
+                        new MigrationMappingStatus(migrationMapping,
+                                MigrationMappingStatusType.Failed, 100, e));
+            }
         }
 
-        public void migrate(MigrationProject migrationProject)
-                        throws DataMigrationException, DataTransporterSendException,
-                        DataLoaderException, DataTransporterRemoveException {
-
-                migrationObserver
-                                .onProjectMigrationStatus(
-                                                new MigrationStatus(migrationProject, MigrationStatusType.InProgress, 0,
-                                                                null));
-
-                for (MigrationMapping migrationMapping : migrationProject.migrationMappings()) {
-                        migrationObserver.onTableMigrationStatus(
-                                        new MigrationMappingStatus(migrationMapping, MigrationMappingStatusType.Started,
-                                                        0, null));
-
-                        try {
-                                migrationObserver.onTableMigrationStatus(
-                                                new MigrationMappingStatus(migrationMapping,
-                                                                MigrationMappingStatusType.InProgress, 0, null));
-                                String sourceTableData = dataExtractor.extractTable(migrationMapping.sourceTable(),
-                                                migrationProject.sourceDataSource());
-
-                                dataTransporter.receiveTableData(sourceTableData, migrationMapping.sourceTable());
-
-                                String targetTableData = dataTransporter.sendTableData(migrationMapping.sourceTable());
-                                dataLoader.loadTable(targetTableData, migrationMapping.targetTable(),
-                                                migrationProject.targetDataSource());
-
-                                dataTransporter.removeTableData(migrationMapping.sourceTable());
-
-                                migrationObserver.onTableMigrationStatus(
-                                                new MigrationMappingStatus(migrationMapping,
-                                                                MigrationMappingStatusType.Successful, 100, null));
-                        } catch (Exception e) {
-                                migrationObserver.onTableMigrationStatus(
-                                                new MigrationMappingStatus(migrationMapping,
-                                                                MigrationMappingStatusType.Failed, 100, e));
-                        }
-                }
-
-                migrationObserver
-                                .onProjectMigrationStatus(
-                                                new MigrationStatus(migrationProject, MigrationStatusType.Successful,
-                                                                100, null));
-        }
+        migrationObserver
+                .onProjectMigrationStatus(
+                        new MigrationStatus(migrationProject, MigrationStatusType.Successful,
+                                100, null));
+    }
 }
