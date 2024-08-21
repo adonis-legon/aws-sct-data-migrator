eda80ea04e42053fdfb62e9e43c575627d83d827
Adonis Cesar Legón Campo <alegon@gmail.com>
2024-08-20 23:35:10 -0400

    minor changes

--- src/test/java/app/alegon/aws/sct/migrator/AwsSctDataMigratorTests.java
+++ src/test/java/app/alegon/aws/sct/migrator/AwsSctDataMigratorTests.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 
 import app.alegon.aws.sct.migrator.business.MigrationService;
